48ef910eb323f894fb72f457fa1a7ba3c56173e5
logeshmohan <42216016+logeshmohan@users.noreply.github.com>
2019-07-01 01:16:21 +0530

    Add files via upload

---
+++ quick.py
@@ -0,0 +1,17 @@
+def sort(array=[]):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            if x == pivot:
+                equal.append(x)
+            if x > pivot:
+                greater.append(x)
+            sort(less)
+            sort(pivot)
+            sort(greater)
